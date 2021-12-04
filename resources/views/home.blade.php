ef81b9332785ea1275cc0aaf6b4297bfbab0f252
cristhyanferrao <71941441+CrisJake@users.noreply.github.com>
2021-12-04 15:47:37 -0300

    alteração na listagem dos serviços

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,8 +3,8 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        
-        <div id="search-container" class="col-md-12">
+    <div class="col text-center">
+        <div id="register-container" class="col-md-12">
             <h1>Registre um Serviço</h1>
             <button onclick="location.href = '/service/new/'" id="myButton" class="btn btn-primary">Registrar</button>
         </div><br>
@@ -12,10 +12,10 @@
             <h1>Busque um Serviço</h1>
             <form action="/service/list" method="GET">
                 <input type="text" id="search" name="search" class="form-control" placeholder="Procurar...">
-                <button type="submit" class="btn btn-primary">Pesquisar</button>
+                <br><button type="submit" class="btn btn-primary">Pesquisar</button>
             </form>
         </div>
-        
+</div>
 
     </div>
 </div>
--- resources/views/services/ListService.blade.php
+++ resources/views/services/ListService.blade.php
@@ -3,17 +3,25 @@
 @section('title', "Busca do Serviço")
 
 @section('content')
-
-@foreach($services as $service)
-<div class="card col-md-3">
-
-    <div class="card-body">
-        <h5 class="card-title"><p>Tipo do serviço prestado: </p>{{ $service->tipo_servico }}</h5>
-        <p class="card-date"><p>Tempo de serviço:</p> {{ date('d/m/Y', strtotime($service->tempo_inicial)) }} - {{ date('d/m/Y', strtotime($service->tempo_final)) }}</p>
-        <h5 class="card-title"><p>Descrição do serviço: </p>{{ $service->descricao }}</h5>
+<div class="col-md-12 col-centered">
+    <h1 style="text-align:center">Serviços</h1>
+    <div class="row">
+        @foreach($services as $service)
+        <div class="col-sm-3">
+            <div class="card text-white bg-dark">
+                <div class="card-body">
+                    <h5 class="card-title"><p><b>Tipo do serviço prestado:</b> </p>{{ $service->tipo_servico }}</h5>
+                     <p class="card-date"><p><b>Tempo de serviço:</b></p> {{ date('d/m/Y', strtotime($service->tempo_inicial)) }} - {{ date('d/m/Y', strtotime($service->tempo_final)) }}</p>
+                    <h5 class="card-title"><p><b>Descrição do serviço:</b> </p>{{ $service->descricao }}</h5>
+                </div>
+            </div>
+        </div>
+        @endforeach
+    </div>
+    <div class="col text-center">
+        <br><button onclick="location.href = '/home/'" id="Button" class="btn btn-success">Voltar</button>
     </div>
 </div>
-@endforeach
 
 @if(count($services) == 0 && $search)
 <p>Não foi possível encontrar nenhum serviço com {{ $search }}</p>
